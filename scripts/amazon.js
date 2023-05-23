dba60e116f15bd93dbfc4f9c98d8d2f809f8524e
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-05-23 05:56:44 -0700

    nake add to cart button interact

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -20,7 +20,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-price">
-        ${(product.priceCent / 100).toFixed(2)}
+        ${(product.priceCents / 100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
@@ -45,7 +45,7 @@ products.forEach((product) => {
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>`;
@@ -54,4 +54,27 @@ products.forEach((product) => {
 
 console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let matching;
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matching = item;
+            }
+        });
+        if (matching) {
+            matching.quanitity += 1;
+        } else {
+            cart.push({
+                productId: productId,
+                quanitity: 1
+            });
+        }
+        
+        console.log(cart);
+    });
+});
